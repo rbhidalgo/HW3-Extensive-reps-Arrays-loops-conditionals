2a3b00509c86e7e7a4475939a81c3366bc9819fa
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-03-20 19:00:36 -0700

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -26,3 +26,26 @@ for (let i = 1; i <= 20; i++) {
         console.log(phrase[Math.floor(Math.random() * 3)]);
     }
 }
+
+// Write a javascript application that logs all numbers from 1 - 100.
+
+// If a number is divisible by 3 log "Fizz" instead of the number.
+
+// If a number is divisible by 5 log "Buzz" instead of the number.
+
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+for (let i = 1; i <= 100; i++) {
+    
+    if (i % 15 === 0) {
+     console.log("FizzBuzz") 
+    } 
+    else if (i % 5 === 0) {
+    console.log("Buzz") 
+    }
+    else if (i % 3 === 0 ) { 
+    console.log("Fizz")
+    } else {
+    console.log(i);
+    }
+}
